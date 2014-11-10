7c553d83dce9d7a8d8f835aaa924a97af8106700
Dylan <dylan@cogolabs.com>
2014-11-10 10:38:49 -0500

    initial lib commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var assign = require('object-assign');
+var EventEmitter = require('events').EventEmitter;
+var CHANGE_EVENT = 'change';
+
+var Store = function(state) {
+  var store = assign({}, EventEmitter.prototype, {
+    handlers: {},
+    callbacks: [],
+    state: state,
+    registerHandler: function(name, method) {
+      if (!method) {
+        throw new Error('Handlers must have a function');
+      }
+      if (this.handlers[name]) {
+        throw new Error('Handler already defined on this store for ' + name);
+      }
+      this.handlers[name] = typeof method === 'function' ? [method] : method;
+      return this;
+    },
+    registerCallback: function(method) {
+      if (!method) {
+        throw new Error('Callbacks must have a function');
+      }
+      this.callbacks.push(method);
+      return this;
+    },
+    currentState: function() {
+      return this.state;
+    },
+    emitChange: function() {
+      this.emit(CHANGE_EVENT);
+    }
+  });
+
+  Influx._stores.push(store);
+  return store;
+};
+
+var Action = function(action) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var handled = false;
+  Influx._stores.forEach(function(store) {
+    if (store.handlers[action]) {
+      handled = true;
+      store.handlers[action].forEach((h) => h.apply(this, args));
+      store.callbacks.forEach((cb) => cb());
+      store.emitChange();
+    }
+  });
+  if (!handled) {
+    throw new Error('Nothing handled this action: ' + action);
+  }
+};
+
+var Influx = {
+  change: CHANGE_EVENT,
+  Store: Store,
+  Action: Action,
+
+  _stores: []
+};
+
+module.exports = Influx;
+
